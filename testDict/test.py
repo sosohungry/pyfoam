7ee769ff72d0f66cea28978bfe709d69684151b0
sosohungry <799238605@qq.com>
2022-01-12 16:27:17 +0800

    check

---
+++ testDict/test.py
@@ -0,0 +1,10 @@
+from PyFoam.RunDictionary.ParsedParameterFile import ParsedParameterFile
+
+file = ParsedParameterFile("controlDict")
+
+
+print(type(file))
+
+print("---------------------------------")
+print(file)
+
